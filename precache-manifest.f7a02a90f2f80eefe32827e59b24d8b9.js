18d48828cb423a5e84bcdd3f38c8e3433e549f13
Zacarias Jorge Torrezão <z.torrezao@gmail.com>
2018-11-28 07:46:59 +0200

    Updates

---
+++ precache-manifest.f7a02a90f2f80eefe32827e59b24d8b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b51914cd2bda25e6cca4",
+    "url": "/robotfriends/static/css/main.91e9eee8.chunk.css"
+  },
+  {
+    "revision": "b51914cd2bda25e6cca4",
+    "url": "/robotfriends/static/js/main.b51914cd.chunk.js"
+  },
+  {
+    "revision": "e950f4733b3912d525f7",
+    "url": "/robotfriends/static/css/1.6d478fcf.chunk.css"
+  },
+  {
+    "revision": "e950f4733b3912d525f7",
+    "url": "/robotfriends/static/js/1.e950f473.chunk.js"
+  },
+  {
+    "revision": "036993615acbccafb03f",
+    "url": "/robotfriends/static/js/runtime~main.03699361.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a3cf83446c88829fe901a04e31866a5e",
+    "url": "/robotfriends/index.html"
+  }
+];
